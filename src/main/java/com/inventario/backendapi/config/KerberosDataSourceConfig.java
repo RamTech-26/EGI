ebdf44706f8071c54bffea2c3e67be20bd32bb7a
matifer <matiferunifersidad@gmail.com>
2026-06-11 16:23:28 -0300

    Agregar KerberosDataSourceConfig y config LDAP/Kerberos en application.yaml

---
+++ src/main/java/com/inventario/backendapi/config/KerberosDataSourceConfig.java
@@ -0,0 +1,46 @@
+package com.inventario.backendapi.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@Profile("kerberos")
+public class KerberosDataSourceConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${java.security.krb5.conf}")
+    private String krb5ConfPath;
+
+    @Value("${java.security.auth.login.config}")
+    private String loginConfPath;
+
+    @Bean
+    public DataSource dataSource() {
+        // Configurar Kerberos para la JVM
+        System.setProperty("java.security.krb5.conf", krb5ConfPath);
+        System.setProperty("java.security.auth.login.config", loginConfPath);
+        System.setProperty("javax.security.auth.useSubjectCredsOnly", "false");
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+        Properties props = new Properties();
+        props.setProperty("integratedSecurity", "true");
+        props.setProperty("authenticationScheme", "JavaKerberos");
+        // TODO: Ajustar SPN y host del SQL Server según datos de P2
+        props.setProperty("serverSpn", "MSSQLSvc/<HOST_SQL_SERVER>:1433");
+        config.setDataSourceProperties(props);
+
+        return new HikariDataSource(config);
+    }
+}
